1b15ac10a18115441739e52ed1e899b8006afd2d
QuanNA <naquan2112@gmail.com>
2016-06-06 23:00:11 +0700

    Hello world

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+spl_autoload_register(function ($classname) {
+    require ("../classes/" . $classname . ".php");
+});
+
+$config['displayErrorDetails'] = true;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "user";
+$config['db']['pass']   = "password";
+$config['db']['dbname'] = "exampleapp";
+
+$app = new \Slim\App(["settings" => $config]);
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('app_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+    $this->logger->addInfo("Something interesting happened");
+
+    return $response;
+});
+$app->run();
+?>
