6e5f8a10f19766e171a51b014efc424ab5e985d8
Nguyen Anh Quan <naquan2112@gmail.com>
2016-06-08 15:18:57 +0700

    jquery lang

--- public/index.php
+++ public/index.php
@@ -18,12 +18,31 @@ $container['logger'] = function($c) {
     return $logger;
 };
 
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+                   $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
     $this->logger->addInfo("Something interesting happened");
 
     return $response;
+});
+
+$app->get('/', function (Request $request, Response $response) {
+    return $response->withRedirect('/html/login.html', 303);
+});
+
+$app->post('/authenticate', function (Request $request, Response $response) {
+    $parsedBody = $request->getParsedBody();
+    $result = array('result' => 'ok', 'url' => '/html/home.html'); // TODO business processing
+    return $response->withJson($result);
 });
 
 $app->run();
